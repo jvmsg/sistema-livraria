8445955596aa6fb7bd05d405de4853013eb6524b
João Gomes <joao.gomes@ddmx.com.br>
2023-01-18 21:09:56 -0300

    Feat(Main) Atualização da Main com construtor padrão das classes

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Caixa caixa = new Caixa(1000.0);
 
-        Jogo jogo = new Jogo();
-        Livro livro = new Livro();
+        Jogo jogo = new Jogo(10.0);
+        Livro livro = new Livro(50.0);
 
         caixa.cadastraProduto(jogo);
         caixa.cadastraProduto(livro);
